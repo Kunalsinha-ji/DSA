770b5ffee6d39de24a2759439471eeaf809d9af9
Kunal Sinha <kunal.sinha@cloudwick.com>
2026-03-12 01:06:18 +0530

    Binary search on numbers hard part-1

--- Binary Search/Binary Search on numbers - HARD/22_SplitArray.cpp
+++ Binary Search/Binary Search on numbers - HARD/22_SplitArray.cpp
@@ -1,6 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+    int countSplit(vector<int> &arr,int maxSum){
+        int subArr = 1;
+        int sum = 0;
+        for(auto it: arr){
+            if(sum+it>maxSum){
+                subArr++;
+                sum = it;
+            }
+            else{
+                sum += it;
+            }
+        }
+        return subArr;
+    }
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin(),nums.end());
+        int high = 0;
+
+        for(auto it: nums){
+            high += it;
+        }
+        int ans = -1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            int subArrays = countSplit(nums,mid);
+
+            if(subArrays>k){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
+
 int main() {
     return 0;
 }
